491a88fea99e1bb49f2eb6900900c8da14d4df06
tbotaq <tbotaq@gmail.com>
2009-10-16 20:22:06 +0900

    solved release memory error.

--- src/templateMatching.cpp
+++ src/templateMatching.cpp
@@ -10,39 +10,27 @@ templateMatching::templateMatching(CvSize srcSize)
   CvSize srcImageSize = srcSize;
   templateImage       = cvLoadImage("todai_boys_small.jpg",1); 
   //memory allocation
-  sourceGrayImage     = cvCreateImage(srcImageSize,IPL_DEPTH_8U,1);
   templateGrayImage   = cvCreateImage(cvGetSize(templateImage),IPL_DEPTH_8U,1);
   sourceBinaryImage   = cvCreateImage(srcImageSize,IPL_DEPTH_8U,1);
   templateBinaryImage = cvCreateImage(cvGetSize(templateImage),IPL_DEPTH_8U,1);
-  differenceMapImage  = cvCreateImage(cvSize(srcImageSize.width - templateImage->width +1,srcImageSize.height -templateImage->height +1),IPL_DEPTH_32F,1);
-  sourceGrayImage     = NULL;
-  
+  differenceMapImage  = cvCreateImage(cvSize(srcImageSize.width - templateImage->width + 1,srcImageSize.height - templateImage->height + 1),IPL_DEPTH_32F,1);
 }
 
 templateMatching::~templateMatching()
-{/*
-   cout<<"A"<<endl;
-   //cvReleaseImage( &sourceImage );
-   cout<<"A2"<<endl;
-   cvReleaseImage( &templateImage );
-   cvReleaseImage( &sourceGrayImage );
-   cout<<"B"<<endl;
-   cvReleaseImage( &templateGrayImage );
-   cvReleaseImage( &sourceBinaryImage );
-   cvReleaseImage( &templateBinaryImage );
-   cvReleaseImage( &differenceMapImage );
-   cout<<"Z"<<endl
- */
+{
+  cvReleaseImage( &templateGrayImage );
+  cvReleaseImage( &sourceBinaryImage );
+  cvReleaseImage( &templateBinaryImage );
+  cvReleaseImage( &differenceMapImage );
 } 
 
 int templateMatching::calcMatchResult( IplImage *sourceImage, CvPoint *center, int *radius)//this function shoud be given sourceImage to process and returns center location and radius of detected face
 {
   //convertion from BGR to gray scale for source and template image
-  sourceGrayImage = sourceImage;
   cvCvtColor( templateImage, templateGrayImage, CV_BGR2GRAY );
 
   //Binarization
-  cvThreshold( sourceGrayImage, sourceBinaryImage, THRESHOLD, THRESHOLD_MAX_VALUE, CV_THRESH_BINARY );
+  cvThreshold( sourceImage, sourceBinaryImage, THRESHOLD, THRESHOLD_MAX_VALUE, CV_THRESH_BINARY );
   cvThreshold( templateGrayImage, templateBinaryImage, THRESHOLD, THRESHOLD_MAX_VALUE, CV_THRESH_BINARY );
       
   //calculate the similarity by "SSD",which returns minimum value as most resembled value   
