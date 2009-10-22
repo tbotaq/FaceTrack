b15d5aded96d095bf8f8c31a99463aa56bffa50b
tbotaq <tbotaq@gmail.com>
2009-10-22 19:28:08 +0900

    anyway.

--- src/faceTrack.cpp
+++ src/faceTrack.cpp
@@ -29,6 +29,12 @@
 using namespace std;
 using namespace point;
 
+
+void PPP(string string)
+{
+  cout<<string<<endl;
+}
+
 //declaration of interfaces for each class
 cameraImages *ci;
 faceDetector *fd;
@@ -68,6 +74,7 @@ struct thread_arg
   int dst_x; //destination point
   int dst_y; //destination point
   IplImage *templateImage;
+  bool detectedAbnormalNum;
 };
 
 
@@ -78,7 +85,7 @@ void *thread_move(void *_arg_m)//thread for move control
   struct thread_arg *arg_m;
   arg_m = (struct thread_arg *)_arg_m;
   ptu->move(arg_m->pan, arg_m->tilt);
-  pthread_mutex_unlock(&mutex); 
+     pthread_mutex_unlock(&mutex); 
 }
 
 //the thread for face detection method
@@ -86,6 +93,11 @@ void *thread_facedetect(void *_arg_f)//thread for image proccessing
 {
   pthread_mutex_lock(&mutex);
   struct thread_arg *arg_f;
+
+  double prevX=0,prevY=0;
+  int diffX,diffY;
+  bool updatedCenterLoc = 0;
+
   arg_f=(struct thread_arg *)_arg_f;
 
   //setting center of window as destination
@@ -95,20 +107,35 @@ void *thread_facedetect(void *_arg_f)//thread for image proccessing
   //acquire current image 
   ci->acquire();
 
+  if(updatedCenterLoc)
+    {
+      prevX = arg_f->center.x;
+      prevY = arg_f->center.y;
+    }
+
   //calculate the center location and radius of matched face
   tmch -> calcMatchResult(ci -> getIntensityImg(),arg_f->templateImage,ci->getImageSize(),&arg_f->center,&arg_f->radius);
+  updatedCenterLoc = 1;
 
   //calculate the distance between face's center location and destination
   arg_f -> dX = dist(arg_f -> center.x, arg_f -> dst_x);
   arg_f -> dY = dist(arg_f -> center.y, arg_f -> dst_y);
     
-  //drow a circle on the detected face and  line from face's center location to destination
+  //drow a circle on the detected face,line from face's center location to destination
   cvCircle(ci->getIntensityImg(),cvPoint(arg_f -> center.x,arg_f -> center.y),arg_f -> radius,CV_RGB(255,255,255),3,8,0);
   cvLine(ci->getIntensityImg(),cvPoint(arg_f -> dst_x,arg_f -> dst_y),cvPoint(arg_f -> center.x,arg_f -> center.y),CV_RGB(255,255,255),3,8,0);  
 
-  double DISTANCE = sqrt(pow(arg_f->dX,2)+pow(arg_f->dY,2));
-
-  if(DISTANCE<10)
+  diffX = abs(arg_f->center.x - prevX);
+  diffY = abs(arg_f->center.y - prevY);
+  //printf("Input diff>");
+  //scanf("%d",&diffX);
+  //diffY = diffX;
+  cout<<"diff="<<diffX<<","<<diffY<<endl;
+  arg_f->detectedAbnormalNum = false;
+  if(diffX>10 || diffY>10)
+    arg_f->detectedAbnormalNum = true;
+  
+  if(arg_f->detectedAbnormalNum == false)
     {
       //define how long PT unit make movement
       arg_f -> pan = arg_f -> dX;
@@ -120,22 +147,20 @@ void *thread_facedetect(void *_arg_f)//thread for image proccessing
       arg_f -> pan = 0;
       arg_f -> tilt = 0;
     }
-
-  //create a new template image using the acquired data
-  
-  
-  cvWaitKey(1);
+ 
   pthread_mutex_unlock(&mutex);
 }
 
 
 int main(void)
 {
+  PPP("0");
   //reference to each function
   ptu = new panTiltUnit();
+  PPP("1");
   ci = new cameraImages();
   tmch = new templateMatching();
-
+  PPP("2");
   //for time measurement
   double t1=0,t2=0;
   double totalTime=0;
@@ -148,18 +173,18 @@ int main(void)
 
   // initialize camera image class
   ci->initialize();
-
+  PPP("5");
   //estimate the size of image
   ci->acquire();
   CvSize size = ci -> getImageSize();
- 
+  PPP("6");
   //pass the image size to class faceDetector's constructor
   fd = new faceDetector(size);
-
+  PPP("7");
   //define the size of recognitive region using "size"
   arg.Lx = size.width;
   arg.Ly = size.height;
-
+  PPP("10");
   //initialize mutex
   pthread_mutex_init(&mutex,NULL);
    
@@ -167,13 +192,18 @@ int main(void)
   cvNamedWindow("Face Detection", CV_WINDOW_AUTOSIZE);
   cvNamedWindow("Temp", CV_WINDOW_AUTOSIZE);
 
+
+ INITIALIZATION://LABEL
+
+  cout<<"SYSTEM:INITIALIZATION"<<endl;
+  
   int faces = 0;
   bool hasCreatedTemp = false;
       
   //-----acquire the template image to use in the first-----//  
   while(!hasCreatedTemp)
     {
-      cout<<"SYSTEM:\tPlease show your face to the camera."<<endl;
+      cout<<"\tPlease show your face to the camera."<<endl;
       
       while(faces<1)
 	{
@@ -190,16 +220,16 @@ int main(void)
   cvShowImage("Template Image",arg.templateImage);
   
   cout<<"\n\tOK.Now created first template image."<<endl;
-  cout<<"\tPress any key to destroy window."<<endl;
-  //cvShowImage("Temp",arg.templateImage);
-  //cvWaitKey(0);
-  //cvDestroyWindow("Temp");
-
+    
   cout<<"Biclops:"<<endl;
+
+
   //-----main procces-----//
   
   while(1)
     {	 
+
+      PPP("15");
       //ID for move thread and face detection thread
       pthread_t thread_m,thread_f;
 
@@ -208,7 +238,7 @@ int main(void)
       // create threads
       pthread_create(&thread_f, NULL, thread_facedetect, (void *)&arg);
       pthread_create(&thread_m, NULL, thread_move, (void *)&arg);
-     
+      PPP("19");
       //say goodbye to created threads	
       pthread_join(thread_m, NULL);
       pthread_join(thread_f,NULL);
@@ -217,6 +247,12 @@ int main(void)
       t2 = getrusageSec(); 
       totalTime += t2-t1;
       times ++;
+      PPP("20");
+      if(arg.detectedAbnormalNum)
+	{
+	  cout<<"SYSTEM:\tDetected abnormal data.Goto Initialization phase."<<endl;
+	  goto INITIALIZATION;
+	}
 
       // show images
       cvShowImage("Face Detection",ci->getIntensityImg());
@@ -231,7 +267,6 @@ int main(void)
     }
   printf("\nAverage time is %f[sec/process]\n(calculated by %d processes)\n\n",totalTime/times,times);
  
-
   // release memory
   pthread_mutex_destroy(&mutex);
   cvDestroyWindow("Face Detection");
@@ -239,6 +274,6 @@ int main(void)
   delete ci;
   delete ptu;
   delete tmch;
- 
+  PPP("100");
   return 0;
 }
